7114a8b5171c4e8f1474cb00343f95abe3c6498d
@Brandon Mwinga <imotongori@kabarak.ac.ke>
2022-03-10 16:24:37 +0300

    modified some modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import profile from "./image/a.png";
+import email from "./image/email.jpg";
+import pass from "./image/pass/png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className='container-image'>
+              <img src={profile} alt="profile" className='profile' />
+
+            </div>
+
+          </div>
+          <div>
+            <h1>Login page</h1>
+            <div>
+              <img src={email} alt="email" className='email'/>
+              <input type="text" placeholder='user name' className='name'/>
+            </div>
+            <div>
+              <img src={pass} alt="pass" className='pass'/>
+              <input type="text" placeholder='user name' className='name'/>
+            </div>
+            <button>login</button>
+          </div>
+        </div>
+    
+      </div>
     </div>
   );
 }
